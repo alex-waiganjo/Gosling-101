21f7d9c4193416835c5a3c64d3bbf9b1d7440276
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-05-19 22:28:52 +0300

    Learning Java

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            // User Input
+            System.out.println("Your Programming Language of Interest:");
+            String prog_lang = scanner.nextLine();
+
+            // Output
+            System.out.println(prog_lang + " Dev Loading...");                        
+        
+        }        
+        
+    }
+}
